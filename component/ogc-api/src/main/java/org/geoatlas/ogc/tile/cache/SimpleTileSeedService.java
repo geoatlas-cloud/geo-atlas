6073274d17347785e0ef8a83882b1c77837293c5
Fuyi Atlas <thread.zhou@gmail.com>
2024-06-06 11:22:00 +0800

    :sparkles: feat: 完成接入FeatureLayer下的TileMatrixSubset的truncate bbox测试[3857&4490]

--- component/ogc-api/src/main/java/org/geoatlas/ogc/tile/cache/SimpleTileSeedService.java
+++ component/ogc-api/src/main/java/org/geoatlas/ogc/tile/cache/SimpleTileSeedService.java
@@ -4,23 +4,21 @@ import org.geoatlas.cache.core.GeoAtlasCacheException;
 import org.geoatlas.cache.core.seed.SeedRequest;
 import org.geoatlas.cache.core.seed.TileBreeder;
 import org.geoatlas.cache.core.service.AbstractTileSeedService;
-import org.geoatlas.cache.core.storage.StorageBroker;
 import org.geoatlas.metadata.helper.FeatureSourceHelper;
 import org.geoatlas.metadata.model.FeatureLayerInfo;
 import org.geoatlas.metadata.model.NamespaceInfo;
 import org.geoatlas.ogc.tile.context.FeatureTileMatrixSubsetContext;
 import org.geoatlas.pyramid.index.TileMatrixSet;
 import org.geoatlas.pyramid.index.TileMatrixSubset;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 
 /**
+ * 接入TileMatrixSubset, 如果FeatureLayer存在BBox的话
  * @author: <a href="mailto:thread.zhou@gmail.com">Fuyi</a>
  * @time: 2024/6/4 21:55
  * @since: 1.0
  **/
 @Component
-@ConditionalOnBean(value = StorageBroker.class)
 public class SimpleTileSeedService extends AbstractTileSeedService {
 
     private final FeatureTileMatrixSubsetContext subsetContext;
