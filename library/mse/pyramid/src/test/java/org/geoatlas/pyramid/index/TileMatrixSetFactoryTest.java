bce1056b2f6d5a7b67bc9a315e0eabd12f6a283f
Fuyi Atlas <thread.zhou@gmail.com>
2024-07-11 22:43:57 +0800

    :white_check_mark: test: 补充了TileMatrixSet对4490的测试

--- library/mse/pyramid/src/test/java/org/geoatlas/pyramid/index/TileMatrixSetFactoryTest.java
+++ library/mse/pyramid/src/test/java/org/geoatlas/pyramid/index/TileMatrixSetFactoryTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+import java.util.Arrays;
+
+
 public class TileMatrixSetFactoryTest {
 
     public static CoordinateReferenceSystem EPSG4326_CRS;
@@ -36,6 +39,56 @@ public class TileMatrixSetFactoryTest {
     }
 
     @Test
+    public void createTileMatrixSet1() {
+        TileMatrixSet tileMatrixSet = TileMatrixSetFactory.createTileMatrixSet(
+                "EPSG:4326",
+                EPSG4326_CRS,
+                new BoundingBox(-10.0,-30.0,85.0,21.0),
+                CornerOfOrigin.TOP_LEFT,
+                22,
+                null,
+                0.00028,
+                256,
+                256,
+                false);
+        System.out.println(tileMatrixSet);
+    }
+
+    @Test
+    public void createTileMatrixSet4490() throws FactoryException {
+        TileMatrixSet tileMatrixSet = TileMatrixSetFactory.createTileMatrixSet(
+                "EPSG:4490",
+                CRS.decode("EPSG:4490", true),
+                BoundingBox.WORLD4490,
+                CornerOfOrigin.TOP_LEFT,
+                1.40625d, // 天地图经纬度投影0级的分辨率
+                TileMatrixSetFactory.DEFAULT_LEVELS,
+                TileMatrixSetFactory.EPSG_4326_TO_METERS,
+                TileMatrixSetFactory.CGCS2000_PIXEL_SIZE_METER,
+                256,
+                256,
+                false);
+        System.out.println(tileMatrixSet.getTitle());
+    }
+
+    @Test
+    public void createTileMatrixSet44901() throws FactoryException {
+        TileMatrixSet tileMatrixSet = TileMatrixSetFactory.createTileMatrixSet(
+                "EPSG:4490",
+                CRS.decode("EPSG:4490", true),
+                new BoundingBox(-180,-85,180,85),
+                CornerOfOrigin.TOP_LEFT,
+                TileMatrixSetFactory.DEFAULT_LEVELS,
+                TileMatrixSetFactory.EPSG_4326_TO_METERS,
+                TileMatrixSetFactory.CGCS2000_PIXEL_SIZE_METER,
+                256,
+                256,
+                false);
+        System.out.println(tileMatrixSet.getTitle());
+    }
+
+
+    @Test
     public void testCreateTileMatrixSet() throws FactoryException {
         CoordinateReferenceSystem referenceSystem = CRS.decode("EPSG:4549");
         CoordinateReferenceSystem referenceSystem1 = CRS.decode("EPSG:4549");
@@ -45,4 +98,43 @@ public class TileMatrixSetFactoryTest {
     @Test
     public void getMetersPerUnit() {
     }
+
+    @Test
+    public void closestIndex() throws FactoryException, MatrixMismatchException {
+        TileMatrixSet tileMatrixSet = TileMatrixSetFactory.createTileMatrixSet(
+                "EPSG:4490",
+                CRS.decode("EPSG:4490", true),
+                BoundingBox.WORLD4490,
+                CornerOfOrigin.TOP_LEFT,
+                1.40625d, // 天地图经纬度投影0级的分辨率
+                TileMatrixSetFactory.DEFAULT_LEVELS,
+                TileMatrixSetFactory.EPSG_4326_TO_METERS,
+                TileMatrixSetFactory.CGCS2000_PIXEL_SIZE_METER,
+                256,
+                256,
+                false);
+        long[] index = tileMatrixSet.closestIndex(new BoundingBox(-180.0, -90.0, 180, 90));
+        System.out.println(Arrays.toString(index));
+    }
+
+    @Test
+    public void testClosestRectangle() throws Exception {
+        TileMatrixSet tileMatrixSet = TileMatrixSetFactory.createTileMatrixSet(
+                "EPSG:4490",
+                CRS.decode("EPSG:4490", true),
+                BoundingBox.WORLD4490,
+                CornerOfOrigin.TOP_LEFT,
+                1.40625d, // 天地图经纬度投影0级的分辨率
+                TileMatrixSetFactory.DEFAULT_LEVELS,
+                TileMatrixSetFactory.EPSG_4326_TO_METERS,
+                TileMatrixSetFactory.CGCS2000_PIXEL_SIZE_METER,
+                256,
+                256,
+                false);
+        BoundingBox box = new BoundingBox(-180.0, -90.0, 0.0, 0.0);
+        long[] rectTL = tileMatrixSet.closestRectangle(box);
+
+        System.out.println(Arrays.toString(rectTL));
+    }
+
 }
